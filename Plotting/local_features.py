2b50a1e7d0b640b95d1f4e9b80b5b5d2965c76bb
sjerpsthomas <53572785+sjerpsthomas@users.noreply.github.com>
2025-03-11 15:41:28 +0100

    Fix features, add melodic arc implementation

--- Plotting/local_features.py
+++ Plotting/local_features.py
@@ -29,10 +29,10 @@ def note_density(measures: list[Measure]) -> float:
 
 # INTERVAL FEATURES
 
-def interval_list(measures: list[Measure]) -> list[int]
-    return np.diff(note.note for measure in measures for note in measure.notes)
+def interval_list(measures: list[Measure]) -> list[int]:
+    return np.diff([note.note for measure in measures for note in measure.notes])
 
-def interval_avg(measures: list[Measure]) -> float: return np.average(interval_list(measures))
+def interval_avg(measures: list[Measure]) -> float: return np.average(np.abs(interval_list(measures)))
 def interval_std(measures: list[Measure]) -> float: return np.std(interval_list(measures))
 
 
@@ -40,7 +40,7 @@ def interval_std(measures: list[Measure]) -> float: return np.std(interval_list(
 
 def note_share(measures: list[Measure], pitch_class: int) -> float:
     notes: list[int] = note_list(measures)
-    return len(filter(lambda x: x % 12 == pitch_class)) / len(notes)
+    return sum(note % 12 == pitch_class for note in notes) / len(notes)
 
 def note_root_share(measures: list[Measure], key: int) -> float: return note_share(measures, key % 12)
 
@@ -51,12 +51,37 @@ def note_fifth_share(measures: list[Measure], key: int) -> float: return note_sh
 
 # MELODIC ARC FEATURES
 
+def all_notes(measures: list[Measure]) -> list[MidiNote]:
+    return [replace(note, time=(note.time + i)) for i in range(len(measures)) for note in measures[i].notes]
+
 def melodic_arc_list(measures: list[Measure]) -> list[tuple[float, float]]:
-    return []
+    intervals: list[int] = interval_list(measures)
+    notes: list[MidiNote] = all_notes(measures)
 
-def melodic_arc_size_avg(measures: list[Measure]) -> float: return np.average(width for (width, _) in melodic_arc_list(measures))
+    # Get places where interval switches sign
+    boundaries = np.nonzero(np.diff(intervals))[0] + 1
+    
+    # Build arcs list based on boundaries
+    arcs: list[list[MidiNote]] = []
+    arc_start_index: int = 0
 
-def melodic_arc_size_avg(measures: list[Measure]) -> float: return np.average(height for (_, height) in melodic_arc_list(measures))
+    # Iterate over boundaries
+    boundary: int
+    for boundary in boundaries:
+        arcs.append(notes[arc_start_index:boundary + 1])
+        arc_start_index = boundary
+    
+    # Append last arc
+    arcs.append(notes[arc_start_index:])
+
+    durations: list[float] = [((arc[-1].time + arc[-1].length) - arc[0].time) if arc != [] else 0.0 for arc in arcs]
+    heights: list[float] = [float(np.max([note.note for note in arc]) - np.min([note.note for note in arc])) if arc != [] else 0.0 for arc in arcs]
+
+    return list(zip(durations, heights))
+
+def melodic_arc_duration_avg(measures: list[Measure]) -> float: return np.average([width for (width, _) in melodic_arc_list(measures)])
+
+def melodic_arc_height_avg(measures: list[Measure]) -> float: return np.average([height for (_, height) in melodic_arc_list(measures)])
 
 def extrema_ratio(measures: list[Measure]) -> float:
     # Get number of melodic arcs and note count
@@ -69,20 +94,14 @@ def extrema_ratio(measures: list[Measure]) -> float:
 
 # OTHER METRICS
 
+def ioi_list(measures: list[Measure]) -> list[float]:
+    return np.diff([note.time for note in all_notes(measures)])
+
+def ioi_avg(measures: list[Measure]) -> float: return np.average(ioi_list(measures))
+
 def npvi(measures: list[Measure]) -> float:
-    all_notes: list[MidiNote] = (replace(note, time=(note.time + i)) for i in range(measures) for note in measures[i].notes)
-    note_count: int = len(all_notes)
-
-    time_intervals: list[float] = []
-
-    for i in range(note_count - 1):
-        note: MidiNote = all_notes[i]
-        next_note: MidiNote = all_notes[i + 1]
-
-        time_interval = next_note.time - (note.time + note.length)
-        time_interval = max(time_interval, 0)
-
-        time_intervals.append(time_interval)
+    time_intervals = ioi_list(measures)
+    note_count = len(time_intervals) + 1
     
     return (100 / (note_count - 2)) * sum(
         abs(
